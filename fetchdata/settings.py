849aba6456e3495d3046f2b13ce6f9e850d0ad8d
Jayesh Patil <jayeshpatil8l30@gmail.com>
2024-03-29 17:37:11 +0530

    Changed Database from sqlite to mysql

--- fetchdata/settings.py
+++ fetchdata/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'fetchdata.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django-mysql',
+        'USER':'root',
+        'PASSWORD':'Root@123',
+        'HOST':'localhost', 
+        'PORT': '3306'                  
     }
 }
 
